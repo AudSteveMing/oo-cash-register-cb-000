e0c40287b9221eabf6d1c27f8f879cbaddb7e18d
Steve Cordova <cordovas@bc.edu>
2020-08-05 02:07:25 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,35 @@
+class CashRegister
 
+  attr_accessor :items, :discount, :total, :last_transaction
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+    # @argument = argument
+    # self.discount(discount_amount)
+  end
+
+  # def discount(discount)
+  # end
+
+  # def total
+  #   @total
+  # end
+
+  def add_item(title, price, quantity = 1)
+    # @original_total = @total
+    @total = @total + price * quantity
+    # @quantity = quantity
+  end
+
+  def apply_discount
+  end
+
+  def items
+  end
+
+  def void_last_transaction
+  end
+
+end
